564aa25b3fb578039d238942c97e4d110a3a4fd5
Kyle Deal <kdeal@kyledeal.com>
2022-09-24 00:02:43 -0400

    Add function that splits numbers from their units
    This puts it into a consistent format that, so that we can accept both
    while keeping the logic simple.

---
+++ src/utils.go
@@ -0,0 +1,20 @@
+package ralphred
+
+import "regexp"
+
+var numberWithUnitRegex = regexp.MustCompile("^(?P<number>[0-9.]+)(?P<remaining>[^0-9]+)")
+
+func splitUnitFromNumber(args []string) []string {
+	new_args := []string{}
+	for _, str := range args {
+		match := numberWithUnitRegex.FindStringSubmatch(str)
+		if match == nil {
+			new_args = append(new_args, str)
+		} else {
+			number := match[numberWithUnitRegex.SubexpIndex("number")]
+			remaining := match[numberWithUnitRegex.SubexpIndex("remaining")]
+			new_args = append(new_args, number, remaining)
+		}
+	}
+	return new_args
+}
---
+++ src/utils_test.go
@@ -0,0 +1,30 @@
+package ralphred
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSplitNumbersAndText(t *testing.T) {
+	assertResult := func(input []string, expected []string) {
+		result := splitUnitFromNumber(input)
+		if !reflect.DeepEqual(result, expected) {
+			t.Fatalf("Got %s expected %s", result, expected)
+		}
+	}
+	t.Run("EmptyArgs", func(t *testing.T) {
+		assertResult([]string{}, []string{})
+	})
+	t.Run("NoSplit", func(t *testing.T) {
+		assertResult([]string{"a", "b"}, []string{"a", "b"})
+	})
+	t.Run("OneSplit", func(t *testing.T) {
+		assertResult([]string{"1a", "b"}, []string{"1", "a", "b"})
+	})
+	t.Run("FloatSplit", func(t *testing.T) {
+		assertResult([]string{"1.2a"}, []string{"1.2", "a"})
+	})
+	t.Run("NumberAfterText", func(t *testing.T) {
+		assertResult([]string{"1a", "b2"}, []string{"1", "a", "b2"})
+	})
+}
