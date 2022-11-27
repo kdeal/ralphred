fd6f3c176eb58147aca6dbbf12ca5969b77a7bd3
Kyle Deal <kdeal@kyledeal.com>
2022-11-27 06:40:50 +0000

    Support searching a doc set
    This searches all the entries for a doc set and returns the ones that
    match the search query.

--- src/devdocs.go
+++ src/devdocs.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 const DevdocsBaseUrl string = "https://devdocs.io/"
@@ -18,6 +19,23 @@ type DevdocsDocSet struct {
 	DBSize  int64  `json:"db_size"`
 }
 
+type DevDocsDocEntry struct {
+	Name string `json:"name"`
+	Path string `json:"path"`
+	Type string `json:"type"`
+}
+
+type DevDocsDocType struct {
+	Name  string `json:"name"`
+	Count int64  `json:"count"`
+	Slug  string `json:"slug"`
+}
+
+type DevDocsDocIndex struct {
+	Entries []DevDocsDocEntry `json:"entries"`
+	Types   []DevDocsDocType  `json:"types"`
+}
+
 func fetchDevdocsDocsList() ([]DevdocsDocSet, error) {
 	resp, err := http.Get(fmt.Sprintf("%s/docs/docs.json", DevdocsBaseUrl))
 	if err != nil {
@@ -30,6 +48,20 @@ func fetchDevdocsDocsList() ([]DevdocsDocSet, error) {
 		return nil, err
 	}
 	return docsList, nil
+}
+
+func fetchDevdocsDocIndex(docSlug string) (DevDocsDocIndex, error) {
+	resp, err := http.Get(fmt.Sprintf("%s/docs/%s/index.json", DevdocsBaseUrl, docSlug))
+	if err != nil {
+		return DevDocsDocIndex{}, err
+	}
+	defer resp.Body.Close()
+	var docIndex = DevDocsDocIndex{}
+	err = json.NewDecoder(resp.Body).Decode(&docIndex)
+	if err != nil {
+		return DevDocsDocIndex{}, err
+	}
+	return docIndex, nil
 }
 
 func devdocsListDocs() ([]AlfredItem, error) {
@@ -52,13 +84,57 @@ func devdocsListDocs() ([]AlfredItem, error) {
 	return docItems, nil
 }
 
+func filterEntries(docEntries []DevDocsDocEntry, searchQuery []string) []DevDocsDocEntry {
+	if len(searchQuery) == 0 {
+		return docEntries
+	}
+
+	matchedEntries := []DevDocsDocEntry{}
+	for _, entry := range docEntries {
+		match := true
+		for _, term := range searchQuery {
+			if !strings.Contains(entry.Name, term) {
+				match = false
+				break
+			}
+		}
+		if match {
+			matchedEntries = append(matchedEntries, entry)
+		}
+	}
+	return matchedEntries
+}
+
+func devdocsSearchDoc(docSlug string, searchQuery []string) ([]AlfredItem, error) {
+	docsIndex, err := fetchDevdocsDocIndex(docSlug)
+	if err != nil {
+		return []AlfredItem{}, err
+	}
+	entries := filterEntries(docsIndex.Entries, searchQuery)
+	if len(entries) > 25 {
+		entries = entries[:25]
+	}
+	docItems := make([]AlfredItem, len(entries))
+	for i, entry := range entries {
+		docItems[i] = AlfredItem{
+			UID:          entry.Name,
+			Title:        entry.Name,
+			Subtitle:     entry.Path,
+			Arg:          []string{fmt.Sprintf("%s/%s/%s", DevdocsBaseUrl, docSlug, entry.Path)},
+			Autocomplete: entry.Name,
+		}
+	}
+	return docItems, nil
+}
+
 func devdocsCommand(args []string) ([]AlfredItem, error) {
 	if len(args) == 0 {
 		return devdocsListDocs()
 	}
 
-	resp := []AlfredItem{
-		alfredItemFromString("A response", false),
+	searchQuery := []string{}
+	if len(args) > 1 {
+		searchQuery = args[1:]
 	}
-	return resp, nil
+	return devdocsSearchDoc(args[0], searchQuery)
 }
